228d09b14766900a64765ba1ebcd4a4e5ae5a5e3
Abin <wb519@vip.qq.com>
2024-09-16 11:48:02 +0800

    初始化工程2

---
+++ Qiaopi-common/src/main/java/com/qiaopi/constant/JwtClaimsConstant.java
@@ -0,0 +1,9 @@
+package com.qiaopi.constant;
+
+public class JwtClaimsConstant {
+
+    public static final String USER_ID = "userId";
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+    public static final String NAME = "name";
+}
