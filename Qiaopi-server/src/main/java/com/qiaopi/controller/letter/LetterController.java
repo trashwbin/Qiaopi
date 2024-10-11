e2fdc4ab19d8c65707b552a7684e51fb55e6cd50
Abin <Trashwbin@gmail.com>
2024-10-12 04:31:53 +0800

    查信，定时任务

--- Qiaopi-server/src/main/java/com/qiaopi/controller/letter/LetterController.java
+++ Qiaopi-server/src/main/java/com/qiaopi/controller/letter/LetterController.java
@@ -10,6 +10,7 @@ import com.qiaopi.mapper.PaperMapper;
 import com.qiaopi.result.AjaxResult;
 import com.qiaopi.service.LetterService;
 import com.qiaopi.utils.MessageUtils;
+import com.qiaopi.vo.LetterVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -30,6 +31,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.qiaopi.utils.MessageUtils.message;
+
 @RestController
 @RequestMapping("/letter")
 @Slf4j
@@ -42,39 +45,53 @@ public class LetterController {
     private LetterService letterService;
 
     @PostMapping("/generateLetter")
-    @Operation(summary = "生成字体照片")
+    @Operation(summary = "生成侨批")
     public AjaxResult generateImage(@RequestBody LetterGenDTO letterGenDTO) {
-        log.info("生成字体照片：{}", letterGenDTO);
+        log.info("生成侨批：{}", letterGenDTO);
         String url = letterService.generateImage(letterGenDTO);
-        return AjaxResult.success(MessageUtils.message("letter.generateImage.success"),url);
+        return AjaxResult.success(message("letter.generateImage.success"),url);
     }
 
+    /*
     @GetMapping()
+    @Operation(summary = "发信功能测试")
     public AjaxResult hi(){
         Letter letter = new Letter();
         letter.setRecipientEmail("3348620049@qq.com");
         List<Letter> letters= new ArrayList<>();
         letters.add(letter);
-//        letter.setRecipientEmail("Trashwbin@gmail.com");
-//        letters.add(letter);
         letterService.sendLetterToEmail(letters);
         return AjaxResult.success("hi");
-    }
+    }*/
 
 
     @PostMapping("/sendLetter")
-    @Operation(summary = "生成封面照片")
+    @Operation(summary = "生成封面并发送")
     public AjaxResult CoverGenerieren(@RequestBody LetterSendDTO letterSendDTO) throws IOException {
-        log.info("生成封面照片并发送：{}", letterSendDTO);
-        letterService.sendLetterPre(letterSendDTO);
-        return AjaxResult.success(MessageUtils.message("letter.generateImage.success"));
+        log.info("生成封面并发送：{}", letterSendDTO);
+        LetterVO letterVO = letterService.sendLetterPre(letterSendDTO);
+        return AjaxResult.success(message("letter.send.success"),letterVO);
     }
 
+    @GetMapping("/getMySendLetter")
+    @Operation(summary = "获取我写的侨批")
+    public AjaxResult getMySendLetter() {
+        return AjaxResult.success(message("letter.get.my.send.success"),letterService.getMySendLetter());
+    }
 
+    //获取我收到的侨批
+    @GetMapping("/getMyReceiveLetter")
+    @Operation(summary = "获取我收到的侨批")
+    public AjaxResult getMyReceiveLetter() {
+        return AjaxResult.success(message("letter.get.my.receive.success"),letterService.getMyReceiveLetter());
+    }
 
-
-
-
+    //获取我收到未读的侨批
+    @GetMapping("/getMyNotReadLetter")
+    @Operation(description = "或许首页可以弹出这个未读的侨批", summary = "获取我未读的侨批")
+    public AjaxResult getMyNotReadLetter() {
+        return AjaxResult.success(message("letter.get.my.receive.not.read.success"),letterService.getMyNotReadLetter());
+    }
 
 
 }
